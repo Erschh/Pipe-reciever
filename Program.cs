b6139264268456bd00d213aacebec312b2964fa7
Erschh <tomecsko.ors@freemail.hu>
2024-04-22 12:42:26 +0200

    done?

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Pipes;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Client starting...");
+
+        using(var clientStream=new AnonymousPipeClientStream(PipeDirection.In, args[0]))
+        {
+            using(var reader=new StreamReader(clientStream))
+            {
+                string? message;
+
+                while((message=reader.ReadLine())!="")
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
